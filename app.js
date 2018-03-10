734e061d88b9b70ea5c0894fe747c7ff32a5eaa3
ifeoluwa <iafolabi9@gmail.com>
2018-03-10 23:02:19 +0100

    tweked directory structure a little bit

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 'use strict';
-let express = require('express'), //require express
-    config = require('./server/configure'), //configure serverr
-    app = express(), //invoke express
-    mongoose = require('mongoose');//require mongoose
-console.log(' i work')
-try {
-    // console.log('I get to point 1')
-    app.set('port', process.env.PORT || 3007);//set port to environment port or 3000
-    app.set('views', __dirname + '/views'); //ser views directory
-    config(app); //invoke app config
-    mongoose.connect('mongodb://startupia:FIkk**164499@ds145312.mlab.com:45312/heroku_jrw9gf21',
-        {
-            db: {safe: false}
-        }, (err => {
-            if (err) {
-                throw err;
-            }
-        })
-    )
-    ; //connect to online database
-//mongoose.connect('mongodb://localhost/startupia');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-    mongoose.connection.on('open', function () { //connect to mongoose
-        console.log('Mongoose Connected.'); //log connection message
-    });
+const index = require('./server/routes');
+const users = require('./routes/users');
 
+const app = express();
 
-    app.listen(app.get('port'), function () { //listen to port
-        console.log(app.get('port')); //log port being listened to
-    });
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-} catch (err) {
-    console.log(err)
-}
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
