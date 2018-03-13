e5b7b61a247c8ab0bedfea3cecf1c2c1a32540f5
ifeoluwa <iafolabi9@gmail.com>
2018-03-13 10:07:28 +0100

    adding cors handling

--- app.js
+++ app.js
@@ -22,6 +22,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization,Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use((req, res, next) => {
+  //check for options because of that Google Chrome preflight thingsss
+    if(req.method.toLowerCase()!=='options'){
+        next();
+    }else{
+        return res.status(204).send();
+    }
+    // console.log("user in session: ", req.user);
+
+});
+
+
+
+
 
 app.use('/', index);
 app.use('/users', users);
@@ -35,7 +67,6 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  console.log(err);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
